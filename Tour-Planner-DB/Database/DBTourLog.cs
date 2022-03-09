eea5e5975f57781b82498a3c947dbfac66302d9c
Steve <simanek.s@gmx.at>
2022-03-09 14:27:48 +0100

    Changed Folders in DB

--- Tour-Planner-DB/Database/DBTourLog.cs
+++ Tour-Planner-DB/Database/DBTourLog.cs
@@ -1,23 +1,7 @@
 ﻿namespace Tour_Planner_DB;
 
-public class DBConnection
+public partial class DBConnection
 {
-    private readonly NpgsqlConnection connection = new();
-    private readonly IConfiguration _configuration;
-    public DBConnection(IConfiguration configuration)
-    {
-        _configuration = configuration;
-        string SqlSDataSource = _configuration.GetConnectionString("DBConnection");
-        connection = new(SqlSDataSource);
-    }
-    private void Open()
-    {
-        connection.Open();
-    }
-    private void Close()
-    {
-        connection.Close();
-    }
     public IEnumerable<TourLog>? GetAllTourLogs()
     {
         Open();
