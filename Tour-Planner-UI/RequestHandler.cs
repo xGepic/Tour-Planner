fdd057117c2e51394bc75f76af34ef2c623a1753
AlexanderPloechl <if19b086@technikum-wien.at>
2022-03-18 11:58:44 +0100

    requestHandler v1.0 done

--- Tour-Planner-UI/RequestHandler.cs
+++ Tour-Planner-UI/RequestHandler.cs
@@ -2,50 +2,46 @@
 
 internal static class RequestHandler
 {
+    private static readonly HttpClient _Client = new();
+    private static readonly Uri _BaseUri = new("https://localhost:7122/Tour/");
     public static Tour[]? GetAllTours()
     {
-        using HttpClient client = new();
-        Uri endpoint = new("https://localhost:7122/Tour/GetAll");
-        HttpResponseMessage Response = client.GetAsync(endpoint).Result;
+        Uri endpoint = new(_BaseUri, "GetAll");
+        HttpResponseMessage Response = _Client.GetAsync(endpoint).Result;
         string ResultAsString = Response.Content.ReadAsStringAsync().Result;
         Tour[]? AllTours = JsonConvert.DeserializeObject<Tour[]>(ResultAsString);
         return AllTours;
     }
     //public static Tour? GetTourById()
     //{
-    //    using HttpClient client = new();
-    //    Uri endpoint = new("https://localhost:7122/Tour/GetByID");
-    //    HttpResponseMessage Response = client.GetAsync(endpoint).Result;
+    //    Uri endpoint = new(_BaseUri, "GetByID");
+    //    HttpResponseMessage Response = _Client.GetAsync(endpoint).Result;
     //    string ResultAsString = Response.Content.ReadAsStringAsync().Result;
     //    Tour? Tour = JsonConvert.DeserializeObject<Tour>(ResultAsString);
     //    return Tour;
     //}
     public static bool AddTour()
     {
-        using HttpClient client = new();
-        Uri endpoint = new("https://localhost:7122/Tour/AddTour");
-        HttpResponseMessage Response = client.GetAsync(endpoint).Result;
+        Uri endpoint = new(_BaseUri, "AddTour");
+        HttpResponseMessage Response = _Client.GetAsync(endpoint).Result;
         return Response.IsSuccessStatusCode;
     }
     public static bool UpdateTour()
     {
-        using HttpClient client = new();
-        Uri endpoint = new("https://localhost:7122/Tour/UpdateTour");
-        HttpResponseMessage Response = client.GetAsync(endpoint).Result;
+        Uri endpoint = new(_BaseUri, "UpdateTour");
+        HttpResponseMessage Response = _Client.GetAsync(endpoint).Result;
         return Response.IsSuccessStatusCode;
     }
     public static bool DeleteTour()
     {
-        using HttpClient client = new();
-        Uri endpoint = new("https://localhost:7122/Tour/DeleteTour");
-        HttpResponseMessage Response = client.GetAsync(endpoint).Result;
+        Uri endpoint = new(_BaseUri, "DeleteTour");
+        HttpResponseMessage Response = _Client.GetAsync(endpoint).Result;
         return Response.IsSuccessStatusCode;
     }
     public static bool SaveFile()
     {
-        using HttpClient client = new();
-        Uri endpoint = new("https://localhost:7122/Tour/SaveFile");
-        HttpResponseMessage Response = client.GetAsync(endpoint).Result;
+        Uri endpoint = new(_BaseUri, "SaveFile");
+        HttpResponseMessage Response = _Client.GetAsync(endpoint).Result;
         return Response.IsSuccessStatusCode;
     }
 }
