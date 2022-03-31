f58491d5cff037d48e558d983cb946ca8e996e0e
Steve <simanek.s@gmx.at>
2022-03-31 11:39:09 +0200

    Started csv generation

---
+++ Tour-Planner-Business/CsvGenerator/CSVGenerator.cs
@@ -0,0 +1,23 @@
+﻿namespace Tour_Planner_Business;
+
+public static class CSVGenerator
+{
+    private const string csvFolder = "./MyCSV/";
+    private const string fileName = "[CSV] ";
+    private const string fileEnding = ".csv";
+    public static bool GenerateNewCSV(Tour myTour)
+    {
+        string data;
+        try
+        {
+            string filePath = string.Concat(csvFolder, fileName, myTour.TourName, fileEnding);
+            data = string.Concat(myTour.Id + ",", myTour.TourName);
+            File.WriteAllText(filePath, data);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
