d212f7673bf550f4959058c5ef9d566fe1215052
Steve <simanek.s@gmx.at>
2022-04-07 23:31:38 +0200

    toKM added

--- MapQuestTest/Program.cs
+++ MapQuestTest/Program.cs
@@ -6,16 +6,14 @@ public static class MapQuestTest
     private static readonly HttpClient client = new();
     private static readonly Uri baseUri = new("http://www.mapquestapi.com/directions/v2/");
     private static readonly string parameters = "route?key=yKBh4sWxDYGp5iebnTtjXT4YKHR3KXnE&from=Wien&to=Berlin";
+    private static readonly double toKM = 1.61;
     public static void CallUri()
     {
         Uri endpoint = new(baseUri, parameters);
         HttpResponseMessage Response = client.GetAsync(endpoint).Result;
         var result = JObject.Parse(Response.Content.ReadAsStringAsync().Result);
 
-        //distance
-        double distance = Convert.ToInt32(result.SelectToken("route.distance")) * 1.61;
-
-        //time
+        double distance = Convert.ToInt32(result.SelectToken("route.distance")) * toKM;
         TimeSpan time = TimeSpan.FromSeconds(Convert.ToInt32(result.SelectToken("route.time")));
 
         Console.WriteLine("Distance: " + distance + " km");
